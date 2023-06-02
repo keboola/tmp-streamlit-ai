a01f89bd104c5303efb1a589c823996700f685b6
Jordan Burger <jordanrburger@gmail.com>
2023-06-02 15:20:34 -0400

    added autogpt agen ontop of tooling

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,6 @@ from langchain.chat_models import ChatOpenAI
 from langchain.agents import Tool
 from langchain.chains.conversation.memory import ConversationBufferMemory
 from langchain.agents import initialize_agent
-
 from atlassian import Confluence
 import sys
 import os
@@ -12,11 +11,152 @@ import langchain
 from llama_index.langchain_helpers.agents import IndexToolConfig, LlamaIndexTool
 from llama_index.langchain_helpers.agents import create_llama_chat_agent
 from streamlit_chat import message
+from langchain.experimental.autonomous_agents.autogpt.agent import AutoGPT
+from langchain.chat_models import ChatOpenAI
 
+from langchain.agents.agent_toolkits.pandas.base import create_pandas_dataframe_agent
+from langchain.docstore.document import Document
+import asyncio
+from contextlib import contextmanager
+from typing import Optional
+from langchain.agents import tool
+from langchain.tools.file_management.read import ReadFileTool
+from langchain.tools.file_management.write import WriteFileTool
 
 os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
 os.environ["STORAGE_API_URL"] = st.secrets["STORAGE_API_URL"]
 os.environ["STORAGE_API_TOKEN"] = st.secrets["STORAGE_API_TOKEN"]
+
+llm = ChatOpenAI(model_name="gpt-4", temperature=1.0)
+
+ROOT_DIR = "./data/"
+
+
+@contextmanager
+def pushd(new_dir):
+    """Context manager for changing the current working directory."""
+    prev_dir = os.getcwd()
+    os.chdir(new_dir)
+    try:
+        yield
+    finally:
+        os.chdir(prev_dir)
+
+@tool
+def process_csv(
+    csv_file_path: str, instructions: str, output_path: Optional[str] = None
+) -> str:
+    """Process a CSV by with pandas in a limited REPL.\
+ Only use this after writing data to disk as a csv file.\
+ Any figures must be saved to disk to be viewed by the human.\
+ Instructions should be written in natural language, not code. Assume the dataframe is already loaded."""
+    with pushd(ROOT_DIR):
+        try:
+            df = pd.read_csv(csv_file_path)
+        except Exception as e:
+            return f"Error: {e}"
+        agent = create_pandas_dataframe_agent(llm, df, max_iterations=30, verbose=True)
+        if output_path is not None:
+            instructions += f" Save output to disk at {output_path}"
+        try:
+            result = agent.run(instructions)
+            return result
+        except Exception as e:
+            return f"Error: {e}"
+
+async def async_load_playwright(url: str) -> str:
+    """Load the specified URLs using Playwright and parse using BeautifulSoup."""
+    from bs4 import BeautifulSoup
+    from playwright.async_api import async_playwright
+
+    results = ""
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=True)
+        try:
+            page = await browser.new_page()
+            await page.goto(url)
+
+            page_source = await page.content()
+            soup = BeautifulSoup(page_source, "html.parser")
+
+            for script in soup(["script", "style"]):
+                script.extract()
+
+            text = soup.get_text()
+            lines = (line.strip() for line in text.splitlines())
+            chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+            results = "\n".join(chunk for chunk in chunks if chunk)
+        except Exception as e:
+            results = f"Error: {e}"
+        await browser.close()
+    return results
+
+def run_async(coro):
+    event_loop = asyncio.get_event_loop()
+    return event_loop.run_until_complete(coro)
+
+@tool
+def browse_web_page(url: str) -> str:
+    """Verbose way to scrape a whole webpage. Likely to cause issues parsing."""
+    return run_async(async_load_playwright(url))
+
+
+
+from langchain.tools import BaseTool, DuckDuckGoSearchRun
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+from pydantic import Field
+from langchain.chains.qa_with_sources.loading import load_qa_with_sources_chain, BaseCombineDocumentsChain
+
+def _get_text_splitter():
+    return RecursiveCharacterTextSplitter(
+        # Set a really small chunk size, just to show.
+        chunk_size = 500,
+        chunk_overlap  = 20,
+        length_function = len,
+    )
+
+
+class WebpageQATool(BaseTool):
+    name = "query_webpage"
+    description = "Browse a webpage and retrieve the information relevant to the question."
+    text_splitter: RecursiveCharacterTextSplitter = Field(default_factory=_get_text_splitter)
+    qa_chain: BaseCombineDocumentsChain
+    
+    def _run(self, url: str, question: str) -> str:
+        """Useful for browsing websites and scraping the text information."""
+        result = browse_web_page.run(url)
+        docs = [Document(page_content=result, metadata={"source": url})]
+        web_docs = self.text_splitter.split_documents(docs)
+        results = []
+        # TODO: Handle this with a MapReduceChain
+        for i in range(0, len(web_docs), 4):
+            input_docs = web_docs[i:i+4]
+            window_result = self.qa_chain({"input_documents": input_docs, "question": question}, return_only_outputs=True)
+            results.append(f"Response from window {i} - {window_result}")
+        results_docs = [Document(page_content="\n".join(results), metadata={"source": url})]
+        return self.qa_chain({"input_documents": results_docs, "question": question}, return_only_outputs=True)
+    
+    async def _arun(self, url: str, question: str) -> str:
+        raise NotImplementedError
+      
+query_website_tool = WebpageQATool(qa_chain=load_qa_with_sources_chain(llm))
+
+# Memory
+import faiss
+from langchain.vectorstores import FAISS
+from langchain.docstore import InMemoryDocstore
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.tools.human.tool import HumanInputRun
+
+embeddings_model = OpenAIEmbeddings()
+embedding_size = 1536
+index = faiss.IndexFlatL2(embedding_size)
+vectorstore = FAISS(embeddings_model.embed_query, index, InMemoryDocstore({}), {})
+
+# !pip install duckduckgo_search
+web_search = DuckDuckGoSearchRun()
+
 
 confluence = Confluence(
  url=st.secrets["CONFLUENCE_URL"],
@@ -25,8 +165,8 @@ confluence = Confluence(
 
 
 # Function to translate the user's input into a CQL query
-def generate_cql_query_raw(input_text):
-    return f"text ~ '{input_text}'"
+#def generate_cql_query_raw(input_text):
+#    return f"text ~ '{input_text}'"
 
 
 def generate_cql_query_keywords(input_text):
@@ -50,24 +190,18 @@ def construct_index(input_text):
     st.write('Generating CQL query...')
     st.progress(0.1)
     
-    query_raw = generate_cql_query_raw(input_text)
+    #query_raw = generate_cql_query_raw(input_text)
 
     query_keywords = generate_cql_query_keywords(input_text)
 
     st.write('Query generated!')
     st.progress(0.2)
-
     st.write('Searching Confluence...')
     st.progress(0.3)
-
-    #modify the following logic to first search through the local directory of txt files in the data directory. 
-    #If there are no results, then search through Confluence.
-    # If there are results, then use the results from pages that we already have to construct an index
-    pages_raw = confluence.cql(query_raw)
-
+    
     pages_keywords = confluence.cql(query_keywords)
-
-    pages = pages_raw if len(pages_raw['results']) > len(pages_keywords['results']) else pages_keywords
+    
+    pages = pages_keywords #pages_raw if len(pages_raw['results']) > len(pages_keywords['results']) else pages_keywords
 
     documents = []
    
@@ -102,21 +236,34 @@ def construct_index(input_text):
     return index
 
 def chatbot(input_text):
+
+
     tools = [
-    Tool(
-        name = "GPT Index",
-        func=lambda q: str(construct_index(input_text).as_query_engine().query(q)),
-        description="useful for when you want to answer questions about the internal docs from Confluence. The input to this tool should be a complete english sentence.",
-        return_direct=True
+        web_search,
+        WriteFileTool(root_dir="./data"),
+        ReadFileTool(root_dir="./data"),
+        process_csv,
+        query_website_tool,
+        Tool(
+            name = "Conflu Index",
+            func=lambda q: str(construct_index(input_text).as_query_engine().query(q)),
+            description="useful for when you want to answer questions about the internal docs from Confluence. The input to this tool should be a complete english sentence.",
+            return_direct=True
     )
-    ]
+    # HumanInputRun(), # Activate if you want the permit asking for help from the human
+]
+    agent = AutoGPT.from_llm_and_tools(
+    ai_name="Tom",
+    ai_role="Assistant",
+    tools=tools,
+    llm=llm,
+    memory=vectorstore.as_retriever(search_kwargs={"k": 8}),
+    #human_in_the_loop=True, # Set to True if you want to add feedback at each step.
+    )
+    #agent.chain.verbose = True
     
-    memory = ConversationBufferMemory(memory_key="chat_history")
-    llm = ChatOpenAI(temperature=0)
-
-    agent_executor = initialize_agent(tools, llm, agent="conversational-react-description", memory=memory, verbose=True)
-
-    response = agent_executor.run(input=input_text)
+    
+    response = agent.run([input_text])
     st.progress(1.0)
     st.balloons()
     return response
@@ -131,7 +278,7 @@ def main():
 
 
     def get_text():
-        input_text = st.text_input("You: ", "Hello, how are you?", key="input")
+        input_text = st.text_input("You: ", "Answer the following question: What is the process for a customer to set up BYODB? Provide all relevant citations to confluence docs", key="input")
         return input_text
 
 
